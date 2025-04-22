eed5e2724aad10a97c32f1ae393f0432c1d6b0f7
romendezs <ro.mendez005@gmail.com>
2025-04-21 20:52:50 -0600

    feat: add get characters by id

--- DragonBall.Infrastructure/ExternalServices/DragonBallApi.cs
+++ DragonBall.Infrastructure/ExternalServices/DragonBallApi.cs
@@ -22,16 +22,39 @@ namespace DragonBall.Domain.Models
 
         public async Task<List<Character>> GetCharactersAsync()
         {
-            var response = await _httpClient.GetStringAsync("/api/characters");
+            var response = await _httpClient.GetStringAsync("/api/characters?limit=58");
             var result = JsonConvert.DeserializeObject<IResponse>(response, GetJsonSerializerSettings());
             return result.Items.ToList();
         }
 
         public async Task<Character> GetCharacterByIdAsync(int id)
         {
-            var response = await _httpClient.GetStringAsync($"/characters/{id}");
-            var result = JsonConvert.DeserializeObject<Character>(response, GetJsonSerializerSettings());
-            return result;
+            try
+            {
+                var response = await _httpClient.GetAsync($"/api/characters/{id}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        // Handle 404 Not Found
+                        throw new Exception($"Character with ID {id} was not found.");
+                    }
+                    else
+                    {
+                        // Handle other non-success status codes
+                        var errorContent = await response.Content.ReadAsStringAsync();
+                        throw new Exception($"Error fetching character with ID {id}: {response.StatusCode} - {errorContent}");
+                    }
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Character>(content, GetJsonSerializerSettings());
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception($"Error fetching character with ID {id}: {ex.Message}", ex);
+            }
         }
 
         private static JsonSerializerSettings GetJsonSerializerSettings()
